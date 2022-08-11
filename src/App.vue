acdeeadda16333fc6c97d993a277f02a24cd4871
GAC <gustavo.acevedo@carabineros.cl>
2022-08-11 07:14:01 -0400

    comienzo

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Vue JS</h1>
+    <p>hola mundo</p>
+    <car />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import car from "./components/car"
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    car,
   }
-}
+};
+
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
---
+++ src/components/car.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <h1>coche</h1>
+        <p>marca: {{brand}}</p>
+        <p>modelo: {{model}}</p>
+        <p>color: 
+            <span v-for="(color, index) in colors" :key="index">
+            {{index}}:
+            {{color}} -
+            </span>
+        </p>
+
+        <p>color2:
+            <ul>
+                <li v-for="(color, index) in colors" :key="index">
+                {{index}}:{{color}}
+                </li>
+            </ul>
+
+        </p> 
+        <p>precio : {{price}}</p>
+        {{mensaje.title}} - {{mensaje.texto}}
+    </div>
+</template>
+
+<script>
+
+export default {
+    // data() {
+    //     return {
+    //         brand:"Audi",
+    //         model:"A4",
+    //         color:"azul",
+    //         price:38000
+    //     }
+    // },
+
+    setup() {
+        const brand="Audi";
+        const modelo="A5";
+        const colors=["azul", "morado", "rojo", "amarillo"];
+        const price=45000;
+        const mensaje={
+            title:"estoy bien",
+            texto:"welcome"};
+    
+    return{
+        brand,
+        modelo,
+        colors,
+        price,
+        mensaje,
+    };
+}
+
+};
+</script>
+
+<style>
+</style>
